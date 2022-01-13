66eaf8523cc28a0fce3b084a1f73f9332472a05c
Jeba Schulz J <jeba.schulz.j@accenture.com>
2022-01-13 22:03:18 +0530

    Sample Java app created

---
+++ Java/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello..!! Welcome to my Sample Java Application...........!!!");
+        System.out.println("Author : Jeba Schulz J");
+    }
+}
